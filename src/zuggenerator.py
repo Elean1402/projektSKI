3940c3ac1ab43eb30f294079d71cd823fd48b203
gitArtux <63188475+gitArtux@users.noreply.github.com>
2024-04-29 14:02:36 +0200

    Pawns moves untested

--- src/zuggenerator.py
+++ src/zuggenerator.py
@@ -1,56 +1,56 @@
 import numpy as np
 
 # Red/Alpha pawns
-alpha_pawns = np.uint64(0b0111111001111110) #panws start position
+alpha_p = np.uint64(0b0111111001111110) #panws start position
 
 # Red/Alpha knights
-alpha_knights =np.uint64(0) #knights start position
+alpha_k =np.uint64(0) #knights start position
 
 # Alpha
-alpha = alpha_pawns & alpha_knights
+alpha = alpha_p & alpha_k
 
 # Red/Alpha pawns move
-alpha_pawns_forward = np.uint64(0b0000000001111110111111111111111111111111111111111111111111111111)
-alpha_pawns_right = np.uint64(0b0000000011111110111111101111111011111110111111101111111011111100)
-alpha_pawns_left = np.uint64(0b0000000001111111011111110111111101111111011111110111111100111111)
+alpha_p_forward = np.uint64(0b0000000001111110111111111111111111111111111111111111111111111111)
+alpha_p_right = np.uint64(0b0000000011111110111111101111111011111110111111101111111011111100)
+alpha_p_left = np.uint64(0b0000000001111111011111110111111101111111011111110111111100111111)
 
 # Red/Alpha pawns hit
-alpha_pawns_hit_right = np.uint64(0b0000000011111100111111101111111011111110111111101111111011111110)
-alpha_pawns_hit_left = np.uint64(0b0000000000111111011111110111111101111111011111110111111101111111)
+alpha_p_hit_right = np.uint64(0b0000000011111100111111101111111011111110111111101111111011111110)
+alpha_p_hit_left = np.uint64(0b0000000000111111011111110111111101111111011111110111111101111111)
 
 
 
 # Red/Alpha knights move/hit
-alpha_knights_forward_right = np.uint64(0b0000000000000000111111001111111011111110111111101111111011111110)
-alpha_knights_right = np.uint64(0b0000000011111000111111001111110011111100111111001111110011111100)
-alpha_knights_forward_left = np.uint64(0b0000000000000000001111110111111101111111011111110111111101111111)
-alpha_knights_left = np.uint64(0b0000000000011111001111110011111100111111001111110011111100111111)
+alpha_k_forward_right = np.uint64(0b0000000000000000111111001111111011111110111111101111111011111110)
+alpha_k_right = np.uint64(0b0000000011111000111111001111110011111100111111001111110011111100)
+alpha_k_forward_left = np.uint64(0b0000000000000000001111110111111101111111011111110111111101111111)
+alpha_k_left = np.uint64(0b0000000000011111001111110011111100111111001111110011111100111111)
 
 
 
 # Blue/Beta pawns
-beta_pawns = np.uint64(0b0111111001111110000000000000000000000000000000000000000000000000) #panws start position
+beta_p = np.uint64(0b0111111001111110000000000000000000000000000000000000000000000000) #panws start position
 
 #Beta knights
-beta_knights =np.uint64(0) #knights start position
+beta_k =np.uint64(0) #knights start position
 
 # Alpha
-beta = beta_pawns & beta_knights
+beta = beta_p & beta_k
 
 # Blue/Beta pawns move
-beta_pawns_forward = np.uint64(0b1111111111111111111111111111111111111111111111110111111000000000)
-beta_pawns_left = np.uint64(0b0011111101111111011111110111111101111111011111110111111100000000)
-beta_pawns_right = np.uint64(0b1111110011111110111111101111111011111110111111101111111000000000)
+beta_p_forward = np.uint64(0b1111111111111111111111111111111111111111111111110111111000000000)
+beta_p_left = np.uint64(0b0011111101111111011111110111111101111111011111110111111100000000)
+beta_p_right = np.uint64(0b1111110011111110111111101111111011111110111111101111111000000000)
 
 # Blue/Beta pawns hit
-beta_pawns_hit_right = np.uint64(0b1111111111111110111111101111111011111110111111101111110000000000)
-beta_pawns_hit_left = np.uint64(0b1111111101111111011111110111111101111111011111110011111100000000)
+beta_p_hit_right = np.uint64(0b1111111111111110111111101111111011111110111111101111110000000000)
+beta_p_hit_left = np.uint64(0b1111111101111111011111110111111101111111011111110011111100000000)
 
 # Blue/Beta knights
-beta_knights_forward_right = np.uint64(0b1111111011111110111111101111111011111110111111000000000000000000)
-beta_knights_right = np.uint64(0b1111110011111100111111001111110011111100111111001111100000000000)
-beta_knights_forward_left = np.uint64(0b0111111101111111011111110111111101111111001111110000000000000000)
-beta_knights_left = np.uint64(0b0011111100111111001111110011111100111111001111110001111100000000)
+beta_k_forward_right = np.uint64(0b1111111011111110111111101111111011111110111111000000000000000000)
+beta_k_right = np.uint64(0b1111110011111100111111001111110011111100111111001111100000000000)
+beta_k_forward_left = np.uint64(0b0111111101111111011111110111111101111111001111110000000000000000)
+beta_k_left = np.uint64(0b0011111100111111001111110011111100111111001111110001111100000000)
 
 
 #testboard  = np.uint64(0b0010)
@@ -64,85 +64,112 @@ def print_board(board:np.uint64):
 
 
 def alpha_moves():
+	
+    # Pwans
 	# forward
-	moves = (alpha_pawns & alpha_pawns_forward) << 8
-	forward = moves & (moves ^ (beta_pawns & alpha_knights))
+	moves = (alpha_p & alpha_p_forward) << 8
+	p_forward = moves & (moves ^ (beta & alpha_k))
 
 	# left
-	moves = (alpha_pawns & alpha_pawns_left) << 1
-	left = moves & (moves ^ (beta_pawns & alpha_knights))
+	moves = (alpha_p & alpha_p_left) << 1
+	p_left = moves & (moves ^ (beta & alpha_k))
 
 	# right
-	moves = (alpha_pawns & alpha_pawns_right) >> 1
-	right = moves & (moves ^ (beta_pawns & alpha_knights))
+	moves = (alpha_p & alpha_p_right) >> 1
+	p_right = moves & (moves ^ (beta & alpha_k))
 
 	# hit left
-	#moves = (alpha_pawns & alpha_pawns_hit_left) >> 1
-	#hit_left = moves & 
-
+	moves = (alpha_p & alpha_p_hit_left) >> 1
+	p_hit_left = moves & beta
 
 	# hit right
-	np.bitwise_and(alpha_pawns, alpha_pawns_hit_right)
+	moves = (alpha_p & alpha_p_hit_right) >> 1
+	p_hit_right = moves & beta
+	
+    #Knights
+	#forward left
+
+	
+def beta_moves():
+	# forward
+	moves = (beta_p & beta_p_forward) << 8
+	forward = moves & (moves ^ (alpha & beta_k))
+
+	# left
+	moves = (beta_p & beta_p_left) << 1
+	left = moves & (moves ^ (alpha & beta_k))
+
+	# right
+	moves = (beta_p & beta_p_right) >> 1
+	right = moves & (moves ^ (alpha & beta_k))
+
+	# hit left
+	moves = (beta_p & beta_p_hit_left) >> 1
+	hit_left = moves & beta
+
+	# hit right
+	moves = (beta_p & beta_p_hit_right) >> 1
+	hit_right = moves & beta
 	
 #print_board(testboard)
 
 #alphaboardtests
 '''#Pawns
-print("alpha_pawns_forward")
-print_board(alpha_pawns_forward)
+print("alpha_p_forward")
+print_board(alpha_p_forward)
 
-print("alpha_pawns_right")
-print_board(alpha_pawns_right)
+print("alpha_p_right")
+print_board(alpha_p_right)
 
-print("alpha_pawns_hit_right")
-print_board(alpha_pawns_hit_right)
+print("alpha_p_hit_right")
+print_board(alpha_p_hit_right)
 
-print("alpha_pawns_left")
-print_board(alpha_pawns_left)
+print("alpha_p_left")
+print_board(alpha_p_left)
 
-print("alpha_pawns_hit_left")
-print_board(alpha_pawns_hit_left)'''
+print("alpha_p_hit_left")
+print_board(alpha_p_hit_left)'''
 
 #Knights
-print("alpha_knights_forwar_right")
-print_board(alpha_knights_forward_right)
+print("alpha_k_forwar_right")
+print_board(alpha_k_forward_right)
 
-print("alpha_knights_right")
-print_board(alpha_knights_right)
+print("alpha_k_right")
+print_board(alpha_k_right)
 
-print("alpha_knights_forward_left")
-print_board(alpha_knights_forward_left)
+print("alpha_k_forward_left")
+print_board(alpha_k_forward_left)
 
-print("alpha_knights_left")
-print_board(alpha_knights_left)
+print("alpha_k_left")
+print_board(alpha_k_left)
 
 '''#betatest
 #Pawns
-print("beta_pawns_forward")
-print_board(beta_pawns_forward)
+print("beta_p_forward")
+print_board(beta_p_forward)
 
-print("beta_pawns_right")
-print_board(beta_pawns_right)
+print("beta_p_right")
+print_board(beta_p_right)
 
-print("beta_pawns_hit_right")
-print_board(beta_pawns_hit_right)
+print("beta_p_hit_right")
+print_board(beta_p_hit_right)
 
-print("beta_pawns_left")
-print_board(beta_pawns_left)
+print("beta_p_left")
+print_board(beta_p_left)
 
-print("beta_pawns_hit_left")
-print_board(beta_pawns_hit_left)'''
+print("beta_p_hit_left")
+print_board(beta_p_hit_left)'''
 
 
 #Knights
-print("beta_knights_forwar_right")
-print_board(beta_knights_forward_right)
+print("beta_k_forwar_right")
+print_board(beta_k_forward_right)
 
-print("beta_knights_right")
-print_board(beta_knights_right)
+print("beta_k_right")
+print_board(beta_k_right)
 
-print("beta_knights_forward_left")
-print_board(beta_knights_forward_left)
+print("beta_k_forward_left")
+print_board(beta_k_forward_left)
 
-print("beta_knights_left")
-print_board(beta_knights_left)
+print("beta_k_left")
+print_board(beta_k_left)
