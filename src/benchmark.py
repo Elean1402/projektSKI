6b28056acd2664e6245704091a0b547402ce0ba3
Elean1402 <84944566+Elean1402@users.noreply.github.com>
2024-05-09 13:48:19 +0200

    ignore

--- src/benchmark.py
+++ src/benchmark.py
@@ -1,6 +1,13 @@
 from timeit import repeat
 
-def benchmark(func: str):
+from src import zuggenerator
+from zuggenerator import *
+
+
+def benchmark(func):
     txt = open("benchmark.txt", "a")
     zug_time: float = min(repeat(func, repeat=5, number=1000))
-    txt.write(func + str(zug_time) + "\n")
+    txt.write(str(func) + str(zug_time) + "\n")
+
+
+benchmark(alpha_generation())
