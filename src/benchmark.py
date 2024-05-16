f3e7847a79354d19878663c99fdab3f04899a33c
Elean1402 <eric.lean.maier93@gmail.com>
2024-05-17 01:54:46 +0200

    test cases in json

--- src/benchmark.py
+++ src/benchmark.py
@@ -1,11 +1,8 @@
 from timeit import repeat
 
 
-def benchmark(fen,func):
+def benchmark(fen: str, func):
     txt = open("benchmark.txt", "a")
-    print(str(func))
     zug_time = min(repeat(func, number=1000))
-    print(str(zug_time))
-    func = str(func).split(" ")
-    name = func[1]
-    txt.write(name +" für "+fen+": "+ str(zug_time) + "\n")
+    name: list[str] = str(func).split(" ")
+    txt.write(f'{name[1]} für {fen} ist {zug_time}ms pro 1000 Züge \n')
