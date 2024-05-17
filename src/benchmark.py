92fab56b8de9b6b68debc63626433072de94db05
gitArtux <63188475+gitArtux@users.noreply.github.com>
2024-05-17 10:36:13 +0200

    optimized generation

--- src/benchmark.py
+++ src/benchmark.py
@@ -4,8 +4,18 @@ from timeit import repeat
 def benchmark(fen,func):
     txt = open("benchmark.txt", "a")
     print(str(func))
-    zug_time = min(repeat(func, repeat=100, number=1000))
+    zug_time = min(repeat(func, repeat=100, number=10000))
     print(str(zug_time))
     func = str(func).split(" ")
     name = func[1]
     txt.write(name +" für "+fen+": "+ str(zug_time) + "\n")
+
+
+def benchmark(func):
+    txt = open("benchmark.txt", "a")
+    print(str(func))
+    zug_time = min(repeat(func, repeat=100, number=1000))
+    func = str(func).split(" ")
+    name = func[1]
+    print(str(func)+": "+ str(zug_time))
+    txt.write(name +": "+ str(zug_time) + "\n")
