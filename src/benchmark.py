b133a4c8e033cd5cde3a585b3084f547522ffd10
Elean1402 <eric.lean.maier93@gmail.com>
2024-05-29 22:53:49 +0200

    etc

--- src/benchmark.py
+++ src/benchmark.py
@@ -1,5 +1,7 @@
 from timeit import repeat
-
+import cProfile
+import pstats
+import io
 
 class Benchmark:
     @staticmethod
@@ -18,3 +20,27 @@ class Benchmark:
                 txt.write(f'{func_name} für {fen} ist {zug_time}s (min aus 1000 Zügen) \n')
             else:
                 txt.write(f'{func_name} ist {zug_time}s (min aus 1000 Zügen) \n')
+
+
+    @staticmethod
+    def profile(func_with_args, func_name=""):
+        """
+        Inputs: func_with_args is the function to be profiled with its arguments,
+                func_name is the name of the function as string (optional)
+        Output: writes the profile results to a file
+        
+        Example: Benchmark.profile(lambda: my_function('arg1_value', 'arg2_value'), 'my_function')
+        """
+        pr = cProfile.Profile()
+        pr.enable()
+        func_with_args()
+        pr.disable()
+        s = io.StringIO()
+        sortby = 'cumulative'
+        ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+        ps.print_stats()
+        with open("profile.txt", "a") as txt:
+            txt.write(f'Profile results for {func_name}:\n')
+            txt.write(s.getvalue())
+
+
--- src/eric.py
+++ src/eric.py
@@ -1,13 +1,118 @@
+from moveLib import *
+from evalFunction import *
+from scoreConfig_evalFunc import *
+from gui import Gui
+from gamestate import GameState
 from benchmark import Benchmark
+class AlphaBetaSearch:
+    """
+    This class implements the Alpha-Beta Search algorithm for a given game.
 
+    Attributes:
+        game (dict): The game state.
+        depth (int): The maximum depth of the search tree.
+        alpha (float): The best (highest) score that the maximizing player is assured of.
+        beta (float): The best (lowest) score that the minimizing player is assured of.
+    """
 
-def my_function(arg1, arg2):
-    print(arg1, arg2)
+    def __init__(self, game: dict, depth = 5):
+        """
+        The constructor for AlphaBetaSearch class.
 
+        Parameters:
+            game (dict): The game state.
+            depth (int): The maximum depth of the search tree. Default is 5.
+        """
+        self.game = game
+        self.depth = depth
+        self.game["bitboard"] = GameState.createBitBoardFrom(Gui.fenToMatrix(game["board"]), True)
+        self.alpha = -float('inf')
+        self.beta = float('inf')
+        self.m = MoveLib()
+        self.efblue = EvalFunction(ScoreConfig.Version1(), Player.Blue)
+        self.efred = EvalFunction(ScoreConfig.Version1(), Player.Red)
 
-def main():
-    Benchmark.benchmark(lambda:my_function('arg1_value', 'arg2_value'), 'my_function', 'fen_tstring')
+    def search(self, use_iterative_deepening=False):
+        """
+        The function to start the Alpha-Beta Search.
+    
+        Parameters:
+            use_iterative_deepening (bool): Whether to use Iterative Deepening Depth-First Search. Default is False.
+    
+        Returns:
+            str: The best move.
+        """
+        if use_iterative_deepening:
+            best_move = None
+            for depth in range(1, self.depth + 1):
+                _, move = self.alpha_beta_max(self.alpha, self.beta, depth, self.game, None)
+                if move is not None:
+                    best_move = move
+            return best_move
+        else:
+            _, best_move = self.alpha_beta_max(self.alpha, self.beta, self.depth, self.game, None)
+            return best_move
 
+    def alpha_beta_max(self, alpha, beta, depth_left: int, game: dict, move: str):
+        """
+        The function to find the maximum score and the corresponding move.
 
-if __name__ == '__main__':
-    main()
+        Parameters:
+            alpha (float): The best (highest) score that the maximizing player is assured of.
+            beta (float): The best (lowest) score that the minimizing player is assured of.
+            depth_left (int): The remaining depth of the search tree.
+            game (dict): The game state.
+            move (str): The current move.
+
+        Returns:
+            tuple: The maximum score and the corresponding move.
+        """
+        if depth_left == 0:
+            return 2, move
+
+        scorelist = self.efblue.computeOverallScore(gen, board=game["bitboard"])
+        best_score = alpha
+        best_move = None
+
+        for move in reversed(scorelist):
+            score, _ = self.alpha_beta_min(alpha, beta, depth_left - 1, game, move)
+            if score >= beta:
+                return beta, move  # fail hard beta-cutoff
+            if score > alpha:
+                alpha = score  # alpha acts like max in MiniMax
+                best_move = move
+
+        return alpha, best_move
+
+    def alpha_beta_min(self, alpha, beta, depth_left: int, game: dict, move: str):
+        """
+        The function to find the minimum score and the corresponding move.
+
+        Parameters:
+            alpha (float): The best (highest) score that the maximizing player is assured of.
+            beta (float): The best (lowest) score that the minimizing player is assured of.
+            depth_left (int): The remaining depth of the search tree.
+            game (dict): The game state.
+            move (str): The current move.
+
+        Returns:
+            tuple: The minimum score and the corresponding move.
+        """
+        if depth_left == 0:
+            return 3, move
+
+        scorelist = self.efred.computeOverallScore(gen, board=game["bitboard"])
+        best_score = beta
+        best_move = None
+
+        for move in reversed(scorelist):
+            score, _ = self.alpha_beta_max(alpha, beta, depth_left - 1, game, move)
+            if score <= alpha:
+                return alpha, move  # fail hard alpha-cutoff
+            if score < beta:
+                beta = score  # beta acts like min in MiniMax
+                best_move = move
+
+        return beta, best_move
+    
+    
