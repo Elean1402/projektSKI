685e6ba5434b82d25d826e594f1d4cfba1128906
Shin-Young Hur <fromZeroToInf@outlook.de>
2024-05-31 04:32:31 +0200

    überflüssige Dateien gelöscht

--- src/benchmark.py
+++
@@ -1,8 +0,0 @@
-from timeit import repeat
-
-
-def benchmark(func, fen=""):
-    txt = open("benchmark.txt", "a")
-    zug_time = min(repeat(func, number=1000))
-    name: list[str] = str(func).split(" ")
-    txt.write(f'{name[1]} für {fen} ist {zug_time}ms pro 1000 Züge \n')
