e84d37bb5c48b815ccb0369a498929fb358a9da9
Elean1402 <eric.lean.maier93@gmail.com>
2024-06-03 21:22:27 +0200

    new_alpha_beta
    Co-authored-by: CharlinZentgraf <CharlinZentgraf@users.noreply.github.com>

--- src/new_alpha.py
+++ src/new_alpha.py
@@ -3,13 +3,15 @@ from game import *
 from gameserver import *
 from gui import *
 from scoreConfig_evalFunc import *
+from evalFunction import *
+import time
 
-GET_PLAYER_INDEX = 1  # Replace with the actual index
+GET_PLAYER_INDEX = 1
 GET_BOARD_INDEX = 0
 
 
 class AlphaBetaSearch:
-	def __init__(self, game):
+	def __init__(self, game, time_limit=2):
 		self.moveLib = MoveLib()
 		self.eval = EvalFunction(ScoreConfig.Version1(), Player.Blue)
 		self.game = game
@@ -19,16 +21,31 @@ class AlphaBetaSearch:
 		self.eval = EvalFunction(ScoreConfig.Version1(),
 								 self.player)  # Set this to an instance of your evaluation function
 		self.best_move = None
+		self.time_limit = time_limit 
 
-	def search(self):
-
+	
+	def search(self, iterative_deepening=False):
+		start_time = time.time()
 		init_board(*self.bitboards)
-		result = self.alphaBetaMax(-1000000, 1000000, 4, self.bitboards)
-		self.best_move = [self.moveLib.BitsToPosition(self.best_move[0]),
-						  self.moveLib.BitsToPosition(self.best_move[1])]
+		if iterative_deepening:
+			depth = 1
+			while True:
+				result, temp_move = self.alphaBetaMax(-1000000, 1000000, depth, self.bitboards, start_time)
+				if temp_move is not None:
+					self.best_move = [self.moveLib.BitsToPosition(temp_move[0]),
+										self.moveLib.BitsToPosition(temp_move[1])]
+				if self.is_game_over() or time.time() - start_time > self.time_limit:
+					break
+				depth += 1
+		else:
+			result, temp_move = self.alphaBetaMax(-1000000, 1000000, 2, self.bitboards, start_time)
+			if temp_move is not None:
+				self.best_move = [self.moveLib.BitsToPosition(temp_move[0]),
+									self.moveLib.BitsToPosition(temp_move[1])]
 		return self.best_move
 
-	def alphaBetaMax(self, alpha, beta, depthleft, bitboard_copy):
+
+	def alphaBetaMax(self, alpha, beta, depthleft, bitboard_copy, start_time):
 		self.player = Player.Blue
 		if depthleft == 0:
 			etc = generate_moves(self.player)
@@ -37,62 +54,66 @@ class AlphaBetaSearch:
 			else:
 				points = -10000
 
-			return points
+			return points, None
 		moves = generate_moves(self.player)
 		if len(moves) == 0:
 			scorelist = []
 		else:
+			moves_string = moves_to_string(moves)
+			print("moves", moves_string)
 			scorelist = self.eval.computeOverallScore(moves, board=bitboard_copy)
 
 		for i in range(len(scorelist)):
+			if time.time() - start_time > self.time_limit:
+				return None, None
 			move = scorelist.pop()
-			bitboard_copy = [board.red_p, board.red_k, board.blue_p, board.blue_k]
+			bitboard_copy = [red_p, red_k, blue_p, blue_k]
 			execute_move(self.player, move)
-			score = self.alphaBetaMin(alpha, beta, depthleft - 1, bitboard_copy)
-			print(score)
+			score, _ = self.alphaBetaMin(alpha, beta, depthleft - 1, bitboard_copy, start_time)
 			self.player = Player.Blue
 			takeback(self.player, move)
+
 			if score >= beta:
-				return beta
+				return beta, None
 
 			if score > alpha:
-				self.best_move = move
+				temp_move = move
 				alpha = score
 
-		return alpha
+		return alpha, temp_move
 
-	def alphaBetaMin(self, alpha, beta, depthleft, bitboard_copy):
+
+	def alphaBetaMin(self, alpha, beta, depthleft, bitboard_copy, start_time):
 		self.player = Player.Red
 		if depthleft == 0:
-			# print("rating",self.rating(generate_moves(self.player), bitboard_copy))
 			etc = generate_moves(self.player)
 			points = -self.eval.computeOverallScore(etc, board=bitboard_copy)[0][3]
 			print("points", points)
-			return points
+			return points, None
 		moves = generate_moves(self.player)
 		if len(moves) == 0:
 			scorelist = []
 		else:
+			moves_string = moves_to_string(moves)
+			print("moves", moves_string)
 			scorelist = self.eval.computeOverallScore(moves,board=bitboard_copy)
 
 		for i in range(len(scorelist)):
+			if time.time() - start_time > self.time_limit:
+				return None, None
 			move = scorelist.pop()
-			bitboard_copy = [board.red_p, board.red_k, board.blue_p, board.blue_k]
+			bitboard_copy = [red_p, red_k, blue_p, blue_k]
 			execute_move(self.player, move)
-			score = self.alphaBetaMax(alpha, beta, depthleft - 1, bitboard_copy)
+			score, _ = self.alphaBetaMax(alpha, beta, depthleft - 1, bitboard_copy, start_time)
 			print(score)
 			self.player = Player.Red
 			takeback(self.player, move)
 			if score <= alpha:
-				return alpha
+				return alpha, None
 			if score < beta:
-				self.best_move = move
 				beta = score
 
-		return beta
-
-	def rating(moves, board):
-		return eval.computeOverallScore(moves, board=board)
+		return beta, None
 
 
 def generate_moves(player) -> list:
@@ -102,18 +123,18 @@ def generate_moves(player) -> list:
 	# und ruf alpha_beta_min mit alpha.generation
 
 	if player == Player.Blue:
-		return board.blue_generation()
+		return blue_generation()
 	elif player == Player.Red:
-		return board.red_generation()
+		return red_generation()
 	else:
 		raise ValueError("Player must be either b or r")
 
 
 def execute_move(player, move):
 	if player == Player.Blue:
-		board.blue_move_execution(move[0], move[1])
+		blue_move_execution(move[0], move[1])
 	elif player == Player.Red:
-		board.red_move_execution(move[0], move[1])
+		red_move_execution(move[0], move[1])
 
 
 def takeback(player, move):
@@ -131,7 +152,7 @@ def search(state):
 
 
 if __name__ == '__main__':
-	test7 = "6/8/8/8/2r0b04/1b06/8/6 b"
+	test7 = "6/8/8/8/1r01b04/8/8/6 b"
 	fen, player = test7.split(" ")
 	player = Player.Blue if player == "b" else Player.Red
 	bitboard = GameState.createBitBoardFrom(Gui.fenToMatrix(fen), True)
